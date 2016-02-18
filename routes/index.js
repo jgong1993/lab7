28a8172d31503f9250457c3270bf663e8ffcdec9
Johnson Gong <jogong@ucsd.edu>
2016-02-17 21:07:18 -0800

    Added Experiment Header

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,16 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
-  	res.render('index', projects);
-  	projects["grid"] = false;
+  	var random_num = Math.random();
+	console.log(random_num);
+
+	//if (random_num > 0.5) {
+	if(random_num >= 0){
+	  projects['grid'] = false;
+	  res.render('index', projects);
+	} else {
+	  res.redirect('/grid');
+	}
 };
 
 exports.viewGrid = function(req, res){
